2b8fc78ba497eb88ffe3deca5b6c9e4b774bab3e
martipuchal <147094946+martipuchal@users.noreply.github.com>
2025-08-08 11:55:38 +0200

    Add files via upload

--- train_algorithm.sh
+++ train_algorithm.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
+if [ "$1" == "-h" ]; then
+  echo "Usage: ` $0` [Normal file path] [Tumoral file path]"
+  exit 0
+fi
+
 
 # Files:
-normal="train/Normal_nogenes.csv"
-tumor="train/Tumoral_nogenes.csv"
+normal= $1 #"train/Normal_nogenes.csv" 
+tumor=$2 #"train/Tumoral_nogenes.csv"
 
 
 ## XGBoost parameters
-d=9 # max_depth
-c=20	 # min_child_weight
-t="hist" # tree_method
+d=10 # max_depth
+c=1	 # min_child_weight
+
 
 
 # Vector building with the negative(no-oncogenic) and positives(oncogenic) fusions.
 echo "## Vector"
-python "main/main.py" $normal "tmp/normal_vector" -v > "log.log"
-python "main/main.py" $tumor "tmp/tumor_vector" -v -t "AVG">> "log.log"
+python "main/Vec_gen.py" $normal "tmp/Normal_vector" > "log.log"
+python "main/Vec_gen.py" $tumor "tmp/Tumor_vector" >> "log.log"
 
 
 # Build the classifier with the vectors previously build
 echo "## Build" 
-python "main/binari_tensor.py" -d $d -c $c -t $t >> "log.log"
+python "main/binari_tensor.py" -d $d -c $c  >> "log.log"
 
 # Run the evaluation of the models
 echo "## Eval" 
-python "main/Evaluate_models.py" >> "log.log"
+python "main/Multy_ROC.py" >> "log.log"
 
 # Clean the tmp files
 rm tmp/*_vector
-rm best_model.h5
 
 
 
