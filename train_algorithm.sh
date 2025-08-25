e9d97e00ece99b1dafbe1fd11ed7261b70db528b
martipuchal <147094946+martipuchal@users.noreply.github.com>
2025-08-25 11:23:01 +0200

    Add files via upload

--- train_algorithm.sh
+++ train_algorithm.sh
@@ -1,12 +1,14 @@
-usage="$(basename "Trai_algorithm") [-h] [-n path/of/Normal_file] [-t path/of/Turmoral_file]  -- Script to re-train the different models to perform the classification.\n
+usage="$(basename "Trai_algorithm") [-h] [-n path/of/Normal_file] [-t path/of/Turmoral_file] [-s] -- Script to re-train the different models to perform the classification.\n
 \n
 where:\n
     -h  show this help text\n
-    -s  File and path to the normal tissue fusions\n
-    -t  File and path to the tumoral tissue fusions\n"
+    -n  File and path to the normal tissue fusions\n
+    -t  File and path to the tumoral tissue fusions\n
+    -s	Flag to not save the models\n"
 
 declare -i helpval=0
-while getopts ":h:n:t:" opt; do
+save="False"
+while getopts ":h:n:t:s" opt; do
   case $opt in
     h) echo $usage
        exit
@@ -17,32 +19,33 @@ while getopts ":h:n:t:" opt; do
     t) tumor=${OPTARG}
        helpval+=1
       ;;
+    s) save="True"
+      ;;
   esac
 done
 
-echo $helpval
-if [ ${helpval} = 2 ]; then
 
-## XGBoost parameters
-d=10 # max_depth
-c=1	 # min_child_weight
+if [ ${helpval} = 2 ]; then
 
 # Vector building with the negative(no-oncogenic) and positives(oncogenic) fusions.
 echo "## Vector"
-python "main/Vec_gen.py" $normal "tmp/Normal_vector" > "log.log"
-python "main/Vec_gen.py" $tumor "tmp/Tumor_vector" >> "log.log"
+python "main/Vec_gen.py" $normal "tmp/normal_vector_train" > "log.log"
+python "main/Vec_gen.py" $tumor "tmp/tumor_vector_train" >> "log.log"
 
 
 # Build the classifier with the vectors previously build
-echo "## Build" 
-python "main/binari_tensor.py" -d $d -c $c  >> "log.log"
-
 # Run the evaluation of the models
-echo "## Eval" 
-python "main/Multy_ROC.py" >> "log.log"
+echo "## Build and Eval" 
+python "main/train_algorithms.py" -s $save >> "log.log"
+
+# Generate the markdown
+echo "## Report"
+R -e "rmarkdown::render('main/Report.Rmd')" >>"log.log"
 
 # Clean the tmp files
-rm tmp/*_vector
+mv main/Report.html .
+#rm tmp/*
+
 
 
 
